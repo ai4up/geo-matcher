3e9f25621389c456b07b3da8b48577833036bf50
Florian Nachtigall <nachtigall.florian@gmail.com>
2025-02-12 17:10:43 +0100

    Use building index for state and URL management
    We now use the alphanumeric building index (hereafter called id) instead
    of the positional index to hold state and navigate to the next candidate
    in the browser. This allows a specific building of interest to be
    displayed directly using the building id.
    
    - Make the id path argument optional for the /show_candidate endpoint -
      instead, automatically show the next candidate.
    - Now iterates over unlabeled candidates until all are labeled, making
      the State singleton the single source of truth.
    - Adjusted templates and map rendering to use building ids instead of
      positional indices.

--- eubucco_conflator/app.py
+++ eubucco_conflator/app.py
@@ -19,10 +19,9 @@ maps_dir = Path(app.static_folder) / "maps"
 
 def start():
     _clean_maps_dir()
-    _create_html(0)
     atexit.register(s.store_results)
 
-    webbrowser.open('http://127.0.0.1:5001/show_candidate/0')
+    webbrowser.open(f"http://127.0.0.1:5001/show_candidate")
     serve(app, host="127.0.0.1", port=5001)
 
 
@@ -34,37 +33,47 @@ def home():
 @app.route('/store-label', methods=['POST'])
 def store_label():
     data = request.json
-    i = data.get("i")
 
-    id = s.candidates.iloc[i].name
+    id = data.get("id")
     label = data.get("label")
     existing_id = data.get("existing_id")
     s.add_result(id, label, existing_id)
 
-    return jsonify({"message": "Success", "next_candidate": i + 1})
+    return jsonify({"message": "Success", "candidate": s.current_candidate_id()})
 
 
-@app.route("/show_candidate/<int:i>")
-def show_candidate(i):
-    if i >= len(s.candidates):
+@app.route("/show_candidate")
+@app.route("/show_candidate/<id>")
+def show_candidate(id=None):
+    if id is None:
+        id = s.current_candidate_id()
+
+    if id is None:
         s.store_results()
         return f"All buildings labeled! Results stored in {RESULTS_FILE}", 200
 
-    next_html = maps_dir / f"candidate_{i+1}.html"
-    if not next_html.is_file():
-        app.logger.debug(f"Pre-generating HTML map for candidate {i+1}")
-        executor.submit(_create_html, i+1)
+    if id not in s.candidates.index:
+        return "Candidate not found", 404
 
-    return render_template("show_candidate.html", label_function_script=_js_labeling_func(), i=i)
+    _create_html(id)
+
+    if next_id := s.next_candidate_id():
+        app.logger.debug(f"Pre-generating HTML map for candidate {next_id}")
+        executor.submit(_create_html, next_id)
+
+    return render_template("show_candidate.html", label_function_script=_js_labeling_func(), id=id)
 
 
-def _create_html(i):
-    if i >= len(s.candidates):
-        app.logger.info(f"HTML maps have already been generated for all s.candidates.")
+def _html_exists(id):
+    return (maps_dir / f"candidate_{id}.html").is_file()
+
+
+def _create_html(id):
+    if _html_exists(id):
         return
 
-    candidate = s.candidates.iloc[i]
-    gdf = s.gdf[s.gdf['candidate_id'] == candidate.name]
+    candidate = s.candidates.loc[id]
+    gdf = s.gdf[s.gdf['candidate_id'] == id]
 
     # Create Folium map centered on the candidate
     with warnings.catch_warnings():
@@ -75,14 +84,14 @@ def _create_html(i):
     # Add existing buildings to the map
     gdf_neighbors_existing = gdf[gdf['dataset'] != candidate.dataset]
     for _, row in gdf_neighbors_existing.iterrows():
-        html_str = f"<button onclick=\"labelPair({i}, 'yes', '{row.name}')\">Duplicated</button>"
+        html_str = f"<button onclick=\"labelPair('{id}', 'yes', '{row.name}')\">Duplicated</button>"
         html = folium.Html(html_str, script=True)
         popup = folium.Popup(html, max_width=300)
         coords = _lat_lon(row.geometry)
         folium.Polygon(coords, popup=popup, color='skyblue', fill=True, fill_opacity=0.5).add_to(m)
 
     # Add new buildings to the map (for reference)
-    gdf_neighbors_new = gdf[(gdf['dataset'] == candidate.dataset) & (gdf.index != candidate.name)]
+    gdf_neighbors_new = gdf[(gdf['dataset'] == candidate.dataset) & (gdf.index != id)]
     folium.GeoJson(gdf_neighbors_new, style_function=lambda _: {'color': 'coral', 'fillOpacity': 0.2}).add_to(m)
 
     # Highlight the candidate building
@@ -92,19 +101,20 @@ def _create_html(i):
     # Add the script to the map’s HTML
     m.get_root().html.add_child(folium.Element(_js_labeling_func()))
 
-    m.save(maps_dir / f"candidate_{i}.html")
+    m.save(maps_dir / f"candidate_{id}.html")
 
 
 def _js_labeling_func():
     func = """
     <script>
         // Global function accessible from popups
-        function labelPair(i, label, existing_id) {
+        function labelPair(id, label, existing_id) {
+            console.log("Saved id:", id);
             fetch('/store-label', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({
-                    i: i,
+                    id: id,
                     label: label,
                     existing_id: existing_id
                 })
@@ -112,7 +122,7 @@ def _js_labeling_func():
             .then(response => response.json())
             .then(data => {
                 console.log("Saved:", data);
-                window.location.href = `/show_candidate/${data.next_candidate}`;
+                window.location.href = `/show_candidate/${data.candidate}`;
             });
         }
     </script>
