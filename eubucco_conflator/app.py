67d7b9f70f6860ff120960e279c649d6f7f4e22d
wagnerfe <wagner@mcc-berlin.net>
2025-02-17 17:02:30 +0100

    added table

--- eubucco_conflator/app.py
+++ eubucco_conflator/app.py
@@ -7,10 +7,13 @@ import shutil
 from flask import Flask, render_template, request, jsonify
 from flask_executor import Executor
 import folium
+from folium.plugins import MarkerCluster
 from waitress import serve
 
 from eubucco_conflator.state import State as s
 from eubucco_conflator.state import RESULTS_FILE
+
+FT = ['name','address','house_number','country','phone_number']
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
@@ -58,11 +61,26 @@ def show_candidate(id=None):
 
     _create_html(id)
 
+    table_data = [{
+            "base_name": row.base_name,  # or row['base_id'] if that's a column in your DataFrame
+            "base_address": row.base_address,
+            "base_country": row.base_country,
+            "base_house_number": row.base_house_number,
+            "base_normalized_phone": row.base_normalized_phone,
+            "can_name": row.can_name,  # or row['can_id'] if that's a column in your DataFrame
+            "can_address": row.can_address,
+            "can_country": row.can_country,
+            "can_house_number": row.can_house_number,
+            "can_normalized_phone": row.can_normalized_phone,
+            }
+        for idx, row in s.candidates.loc[['08f2ab381ba7615403d9afedf3d41905']].iterrows()]
+        #for idx, row in s.candidates.loc[[id]].iterrows()]  
+
     if next_id := s.next_candidate_id():
         app.logger.debug(f"Pre-generating HTML map for candidate {next_id}")
         executor.submit(_create_html, next_id)
 
-    return render_template("show_candidate.html", label_function_script=_js_labeling_func(), id=id)
+    return render_template("show_candidate.html", label_function_script=_js_labeling_func(), id=id, table_data=table_data)
 
 
 def _html_exists(id):
@@ -80,23 +98,41 @@ def _create_pop_up(row):
 def _create_html(id):
     if _html_exists(id):
         return
-
-    pairs = s.candidates.loc[[id]]
+    
+    #pairs = s.candidates.loc[[id]]
+    pairs = s.candidates.loc[['08f2ab381ba7615403d9afedf3d41905']]
     print('Pairs:',len(pairs))
     base_coords = [pairs.iloc[0]['base_latitude'],pairs.iloc[0]['base_longitude']]
     # Create Folium map centered on base poi
     with warnings.catch_warnings():
         warnings.simplefilter(action='ignore', category=UserWarning)
-        m = folium.Map(location=base_coords,zoom_start=19)
+        m = folium.Map(location=base_coords,zoom_start=19,tiles='Cartodb Positron')
 
-    folium.Marker(base_coords,
-                    icon=folium.Icon(color='green', icon=''),
-                    popup=_create_pop_up(pairs.iloc[0])).add_to(m)            
 
     # display candidates per base_poi
-    for _, row in pairs.iterrows():
-        coords = [row['can_latitude']+1e-5, row['can_longitude']+1e-5]
-        folium.Marker(coords,popup=_create_pop_up(row),icon=folium.Icon(color='red', icon='')).add_to(m)
+    if len(s.candidates) > 1:
+        marker_cluster = MarkerCluster().add_to(m)
+    
+        for _, row in pairs.iterrows():
+            coords = [row['can_latitude']+1e-5, row['can_longitude']+1e-5] # Add a small offset to avoid overlapping markers
+            folium.Marker(coords,
+                        popup=_create_pop_up(row),
+                        icon=folium.Icon(color='blue', icon=''),
+                        opacity=0.8,
+                        ).add_to(marker_cluster)
+
+    else:
+        coords = [pairs['can_latitude']+1e-5, pairs['can_longitude']+1e-5] # Add a small offset to avoid overlapping markers
+        folium.Marker(coords,
+                        popup=_create_pop_up(row),
+                        icon=folium.Icon(color='red', icon=''),
+                        ).add_to(marker_cluster)
+
+
+    folium.Marker(base_coords,
+                    popup=_create_pop_up(pairs.iloc[0]),
+                    icon=folium.Icon(color='purple', icon=''),
+                    ).add_to(m)            
     
     # Add the script to the map’s HTML
     m.get_root().html.add_child(folium.Element(_js_labeling_func()))
