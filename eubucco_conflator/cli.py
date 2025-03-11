6026d629c7f7fc457ab4613bf552ae1caf4323b3
wagnerfe <wagner@mcc-berlin.net>
2025-03-11 17:30:56 +0100

    Uncoment cli code as it is not yet supported for poi matching tool.

--- eubucco_conflator/cli.py
+++ eubucco_conflator/cli.py
@@ -3,45 +3,47 @@ import click
 from eubucco_conflator import dataset, app
 from eubucco_conflator.state import State, CANDIDATES_FILE
 
-@click.group()
-def cli():
-    pass
+# TBD: add cli support for the poi labeling tool
 
-@cli.command()
-@click.argument('filepath', default='candidates.parquet', type=click.Path(exists=True))
-def label(filepath):
-    """
-    Start labeling of duplicated buildings.
+# @click.group()
+# def cli():
+#     pass
 
-    FILEPATH to GeoParquet file containing the potential duplicates to label.
-    """
-    State.init(filepath, logger=click.echo)
-    click.echo(f"Loaded {len(State.gdf)} buildings")
-    click.echo(f"Loaded latest labeling state: {len(State.results)} buildings already labeled")
-    click.echo(f"Starting labeling of {len(State.candidates)} buildings...")
+# @cli.command()
+# @click.argument('filepath', default='candidates.parquet', type=click.Path(exists=True))
+# def label(filepath):
+#     """
+#     Start labeling of duplicated buildings.
 
-    click.echo("Starting browser app...")
-    app.start()
+#     FILEPATH to GeoParquet file containing the potential duplicates to label.
+#     """
+#     State.init(filepath, logger=click.echo)
+#     click.echo(f"Loaded {len(State.gdf)} buildings")
+#     click.echo(f"Loaded latest labeling state: {len(State.results)} buildings already labeled")
+#     click.echo(f"Starting labeling of {len(State.candidates)} buildings...")
 
-
-@cli.command()
-@click.argument('filepath1', required=True, type=click.Path(exists=True))
-@click.argument('filepath2', required=True, type=click.Path(exists=True))
-@click.option('--id-col', default=None, help="Name of the column containing unique building identifiers (default index).")
-@click.option('--min-intersection', '-l', default=0.0, help="Minimum relative overlap for new buildings to be considered for duplicate labeling [0,1).")
-@click.option('--max-intersection', '-u', default=1.0, help="Maximum relative overlap for new buildings to be considered for duplicate labeling (0,1].")
-@click.option('--distance', '-d', default=100, help="Distance threshold for displaying neighboring buildings [meters].")
-def create_labeling_dataset(filepath1, filepath2, id_col, min_intersection, max_intersection, distance):
-    """
-    Create a dataset of potential duplicate buildings.
-
-    FILEPATH1 to GeoParquet file containing the reference (existing) buildings.
-    FILEPATH2 to GeoParquet file containing the new (to be added) buildings.
-    """
-    click.echo("Loading geodata...")
-    dataset.create_duplicate_candidates_dataset(filepath1, filepath2, id_col, (min_intersection, max_intersection), distance, logger=click.echo)
-    click.echo(f"Dataset of duplicate candidates created and stored in {CANDIDATES_FILE}")
+#     click.echo("Starting browser app...")
+#     app.start()
 
 
-if __name__ == '__main__':
-    cli()
+# @cli.command()
+# @click.argument('filepath1', required=True, type=click.Path(exists=True))
+# @click.argument('filepath2', required=True, type=click.Path(exists=True))
+# @click.option('--id-col', default=None, help="Name of the column containing unique building identifiers (default index).")
+# @click.option('--min-intersection', '-l', default=0.0, help="Minimum relative overlap for new buildings to be considered for duplicate labeling [0,1).")
+# @click.option('--max-intersection', '-u', default=1.0, help="Maximum relative overlap for new buildings to be considered for duplicate labeling (0,1].")
+# @click.option('--distance', '-d', default=100, help="Distance threshold for displaying neighboring buildings [meters].")
+# def create_labeling_dataset(filepath1, filepath2, id_col, min_intersection, max_intersection, distance):
+#     """
+#     Create a dataset of potential duplicate buildings.
+
+#     FILEPATH1 to GeoParquet file containing the reference (existing) buildings.
+#     FILEPATH2 to GeoParquet file containing the new (to be added) buildings.
+#     """
+#     click.echo("Loading geodata...")
+#     dataset.create_duplicate_candidates_dataset(filepath1, filepath2, id_col, (min_intersection, max_intersection), distance, logger=click.echo)
+#     click.echo(f"Dataset of duplicate candidates created and stored in {CANDIDATES_FILE}")
+
+
+# if __name__ == '__main__':
+#     cli()
