a4bb394e2d2ddb5d6e5137fedc7b393be5de5307
Florian Nachtigall <nachtigall.florian@gmail.com>
2025-04-29 09:34:20 +0200

    Ensure annotation redundancy env var is an integer

--- geo_matcher/wsgi.py
+++ geo_matcher/wsgi.py
@@ -6,10 +6,13 @@ from geo_matcher.app import create_app
 try:
     data_path = os.environ["DATA_PATH"]
     results_path = os.environ["RESULTS_PATH"]
-    annotation_redundancy = os.environ["ANNOTATION_REDUNDANCY"]
+    annotation_redundancy = int(os.environ["ANNOTATION_REDUNDANCY"])
 
 except KeyError:
     print("Environment variables DATA_PATH, RESULTS_PATH, ANNOTATION_REDUNDANCY and must be set.", file=sys.stderr)
+    sys.exit(1)
+except ValueError:
+    print("Environment variable ANNOTATION_REDUNDANCY must be an integer.", file=sys.stderr)
     sys.exit(1)
 
 app = create_app(data_path, results_path, annotation_redundancy)
